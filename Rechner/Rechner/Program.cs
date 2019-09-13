6f15244a52ca4a4d683c6637749671efca92946a
Manateeee <54984968+Manateeee@users.noreply.github.com>
2019-09-13 12:52:38 +0200

    Plus Rechnen

---
+++ Rechner/Rechner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Was machen?");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            int Ergebnis = zahl1 + zahl2;
+
+            Console.WriteLine(zahl1 + " + " + zahl2 + " = " + Ergebnis);
+            Console.ReadLine();
+
+        }
+    }
+}
