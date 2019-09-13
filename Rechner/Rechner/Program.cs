f234e781e63697e36bc9119367df0fcdf6546bf3
Manateeee <54984968+Manateeee@users.noreply.github.com>
2019-09-13 13:22:27 +0200

    Minus Rechnen

--- Rechner/Rechner/Program.cs
+++ Rechner/Rechner/Program.cs
@@ -10,14 +10,20 @@ namespace Rechner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Was machen?");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Was machen?")
+
+
+        }
+        public int Plus(int zahl1, int zahl2)
+        {
+            zahl1 = Convert.ToInt32(Console.ReadLine());
+            zahl2 = Convert.ToInt32(Console.ReadLine());
             int Ergebnis = zahl1 + zahl2;
 
             Console.WriteLine(zahl1 + " + " + zahl2 + " = " + Ergebnis);
             Console.ReadLine();
 
+            return Ergebnis;
         }
     }
 }
